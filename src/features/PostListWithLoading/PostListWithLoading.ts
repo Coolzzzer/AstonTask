27dd02dec225263d737cabe73407c0bfced5cbdc
Coolzzzer <andrew.zhuk936@gmail.com>
2025-07-26 10:15:50 +0300

    fix 2

--- src/features/PostListWithLoading/PostListWithLoading.ts
+++ src/features/PostListWithLoading/PostListWithLoading.ts
@@ -1,6 +1,5 @@
 import { withLoading } from "../../shared/lib/hoc/withLoading";
-import type { PostListProps, WithLoadingProps } from "../../shared/types/post";
+import type { PostListProps} from "../../shared/types/post";
 import { PostList } from "../../widgets/PostList/PostList";
 
 export const PostListWithLoading = withLoading<PostListProps>(PostList);
-export type PostListWithLoadingProps = PostListProps & WithLoadingProps;
--- src/widgets/PostList/PostList.tsx
+++ src/widgets/PostList/PostList.tsx
@@ -1,25 +1,32 @@
-import { useMemo, type FC } from "react";
+import { type FC } from "react";
 import { filterByLength } from "../../features/PostLengthFilter/lib/filterByLength";
 import { CommentList } from "../CommentList/ui/CommentList";
 import { NavLink } from "react-router-dom";
 import type { PostListProps } from "../../shared/types/post";
 
-export const PostList:FC<PostListProps> = ({ posts, minLength}) => {
-  const filteredPosts: { post: string; comment: string; id: number }[] = useMemo(
-    () => filterByLength(posts, minLength),
-    [posts, minLength]
-  );
-        
+export const PostList: FC<PostListProps> = ({ posts, minLength }) => {
+  const filteredPosts: { post: string; comment: string; id: number }[] = filterByLength(posts, minLength);
 
-  return(  
-  <ul>
-    {filteredPosts.map((post, index) =>
-      <div style={{background:"grey", margin:"10px", width:"800px", padding:"10px"}} key={index}>{post.post} 
-        <CommentList comments={post.comment} />
-        <button style={{margin:"5px"}}><NavLink to={`/posts/${post.id}`}>Details</NavLink></button>         
-      </div>
-    )}
-  </ul>
-  )
+  return (
+    <ul>
+      {filteredPosts.map((post, index) => (
+        <div
+          style={{
+            background: "grey",
+            margin: "10px",
+            width: "800px",
+            padding: "10px",
+          }}
+          key={index}
+        >
+          {post.post}
+          <CommentList comments={post.comment} />
+          <button style={{ margin: "5px" }}>
+            <NavLink to={`/posts/${post.id}`}>Details</NavLink>
+          </button>
+        </div>
+      ))}
+    </ul>
+  );
 };
 
